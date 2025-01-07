39db198da482d5f59d3e32e88e917e8f04e349f7
Johann Gaviria <johannmgr@gmail.com>
2025-01-07 13:27:09 -0500

    feat(project): refactorizar el manejo de respuesta de errores

--- apps/core/utils/serializer_validation.py
+++ apps/core/utils/serializer_validation.py
@@ -1,3 +1,7 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+
 def serializer_validation(serializer):
     """
     Función para validar los datos de un serializador.
@@ -5,8 +9,8 @@ def serializer_validation(serializer):
     # Valida los datos enviados
     if not serializer.is_valid():
         # Retorna un mensaje de error si los datos no son válidos
-        return {
+        return Response({
             'status': 'error',
             'message': 'Validation error',
             'errors': serializer.errors
-        }
+        }, status=status.HTTP_400_BAD_REQUEST)
