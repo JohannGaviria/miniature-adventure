dbf117c5f920d29df02ce83d829db5155db7e2d0
Johann Gaviria <johannmgr@gmail.com>
2025-01-07 15:01:23 -0500

    test(users): agregar nuevo test para el endpoint de agregar datos del estudiante

--- apps/users/tests/test_add_student_data.py
+++ apps/users/tests/test_add_student_data.py
@@ -81,6 +81,29 @@ class AddStudentDataTestCase(TestCase):
         self.assertTrue('message' in response.data)
 
 
+    def test_add_student_data_existing_data(self):
+        """
+        Prueba de agregar datos de estudiante con datos existentes.
+
+        Verifica que el endpoint responda con un código de estado
+        400 cuando se intenta agregar datos de estudiantes y ya
+        existen datos de estudiantes para el usuario.
+        """
+        Student.objects.create(
+            university='Test University',
+            degree='Test Degree',
+            major='Test Major',
+            graduation_year=2025,
+            professional_experience='Test Experience',
+            about_me='Test About Me',
+            user=self.user
+        )
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('status' in response.data)
+        self.assertTrue('message' in response.data)
+
+
     def test_add_student_data_without_token(self):
         """
         Prueba de agregar datos de estudiante sin token de autenticación.
