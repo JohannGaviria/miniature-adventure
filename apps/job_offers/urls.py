948ce06bb981191ec7269491ef2a5c822085e915
Johann Gaviria <johannmgr@gmail.com>
2025-01-22 17:54:26 -0500

    feat(job offers): crear endpoint para eliminar una oferta de trabajo

--- apps/job_offers/urls.py
+++ apps/job_offers/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('job_offers/filter', views.filter_job_offers, name='filter_job_offers'),
     path('job_offers/update/<str:job_offer_id>', views.update_job_offer, name='update_job_offer'),
     path('job_offers/close/<str:job_offer_id>', views.close_job_offer, name='close_job_offer'),
+    path('job_offers/delete/<str:job_offer_id>', views.delete_job_offer, name='delete_job_offer'),
 ]
