cf8272db1db76aa2512b1d08c90ddf98941ec37a
Johann Gaviria <johannmgr@gmail.com>
2025-01-21 19:53:55 -0500

    feat(job offers): crear endpoint para actualizar una oferta de trabajo

--- apps/job_offers/urls.py
+++ apps/job_offers/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('job_offers/get/<str:job_offer_id>', views.get_job_offer, name='get_job_offer'),
     path('job_offers/all', views.get_all_job_offers, name='get_all_job_offers'),
     path('job_offers/filter', views.filter_job_offers, name='filter_job_offers'),
+    path('job_offers/update/<str:job_offer_id>', views.update_job_offer, name='update_job_offer'),
 ]
