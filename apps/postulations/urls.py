51ab883a1899bf62a6f7a3cc589a9c25d978c62d
Johann Gaviria <johannmgr@gmail.com>
2025-01-23 10:03:26 -0500

    feat(postulations): crear endpoint para aceptar o rechazar postulacion a una oferta de trabajo

--- apps/postulations/urls.py
+++ apps/postulations/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('postulations/postulate/<str:job_offer_id>', views.postulate_job_offer, name='postulate_job_offer'),
     path('postulations/withdraw/<str:job_offer_id>', views.withdraw_postulation, name='withdraw_postulation'),
     path('postulations/get/<str:job_offer_id>', views.get_postulations, name='get_postulations'),
+    path('postulations/accept_reject/<str:job_offer_id>', views.accept_reject_postulation, name='accept_reject_postulation'),
 ]
